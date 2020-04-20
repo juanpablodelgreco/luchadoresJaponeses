dc95c691b4979ca5da34cd201c0077ce5ae2ca07
juanpablodelgreco <juanpablo.delgreco@hotmail.com>
2020-04-20 08:51:54 -0300

    Clase Luchador
    Se agrego la clase luchador

--- src/src/Luchador.java
+++ src/src/Luchador.java
@@ -1,5 +1,55 @@
 package src;
 
 public class Luchador {
+	private int peso;
+	private int altura;
+	private int domina;
+
+	public Luchador(int peso, int altura) {
+		this.peso = peso;
+		this.altura = altura;
+		this.domina = 0;
+	}
+
+	public boolean domina(Luchador l) {
+		boolean domina = false;
+		if (l.peso < this.peso && l.altura < this.altura) {
+			domina = true;
+		} else if (l.peso < this.peso && l.altura == this.altura) {
+			domina = true;
+		} else if (l.peso == this.peso && l.altura < this.altura) {
+			domina = true;
+		}
+		return domina;
+	}
+
+	@Override
+	public String toString() {
+		return "Luchador [peso=" + peso + ", altura=" + altura + ", domina=" + domina + "]";
+	}
+
+	public int getPeso() {
+		return peso;
+	}
+
+	public void setPeso(int peso) {
+		this.peso = peso;
+	}
+
+	public int getAltura() {
+		return altura;
+	}
+
+	public void setAltura(int altura) {
+		this.altura = altura;
+	}
+
+	public int getDomina() {
+		return domina;
+	}
+
+	public void setDomina(int domina) {
+		this.domina = domina;
+	}
 
 }
