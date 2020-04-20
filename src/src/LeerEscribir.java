9aa27d32413fa81d1ac231162c1c557e77ed2b5b
juanpablodelgreco <juanpablo.delgreco@hotmail.com>
2020-04-20 09:20:56 -0300

    Falla leer archivo

--- src/src/LeerEscribir.java
+++ src/src/LeerEscribir.java
@@ -1,5 +1,38 @@
 package src;
 
-public class LeerEscribir {
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
 
+public class LeerEscribir {
+	
+	public static int leerCantLuchadores(String path) {
+		int cantLuchadores = 0;
+		Scanner sc;
+		try {
+			sc = new Scanner(new File(path));
+			cantLuchadores = sc.nextInt();
+			sc.close();
+			return cantLuchadores;
+		} catch (FileNotFoundException e) {
+			System.out.println("No se pudo abrir el archivo para la cantidad");
+		}
+		return 1;
+	}
+	
+	public static void leerLuchadores(String path, Luchador[] l) {
+		try {
+			int i = 0;
+			Scanner sc = new Scanner(new File(path));
+			sc.nextInt();
+			while (sc.hasNextInt()) {
+				l[i].setAltura(sc.nextInt());
+				l[i].setPeso(sc.nextInt());
+				i++;
+			}
+			sc.close();
+		} catch (FileNotFoundException e) {
+			System.out.println("No se pudo abrir el archivo " + path);
+		}
+	}
 }
--- src/src/Main.java
+++ src/src/Main.java
@@ -2,4 +2,14 @@ package src;
 
 public class Main {
 
+	public static void main(String[] args) {
+		String inputPath = "1.txt";
+		int cantLuchadores = 0;
+		cantLuchadores = LeerEscribir.leerCantLuchadores(inputPath);
+		Luchador luchadores[] = new Luchador[cantLuchadores];
+		LeerEscribir.leerLuchadores(inputPath, luchadores);
+		for(Luchador l : luchadores ) {
+			System.out.println(l);
+		}
+	}
 }
