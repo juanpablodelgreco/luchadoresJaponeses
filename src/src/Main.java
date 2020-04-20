79821db4808744cae872d7841c661a6b8c298382
juanpablodelgreco <juanpablo.delgreco@hotmail.com>
2020-04-20 15:21:51 -0300

    Casos de prueba
    Se implementaron 6 casos de prueba, falta el de fatiga.

--- src/src/Main.java
+++ src/src/Main.java
@@ -3,8 +3,9 @@ package src;
 public class Main {
 
 	public static void main(String[] args) {
-		String inputPath = "1.txt";
-		String outputPath = "1.out";
+		int numeroPrueba = 6;
+		String inputPath = "lote_de_pruebas/Inputs/"+numeroPrueba+".in";
+		String outputPath = "lote_de_pruebas/Outputs/"+numeroPrueba+".out";
 		Luchador[] luchadores = null;
 		luchadores = LeerEscribir.leerLuchadores(inputPath);
 		for (int i = 0; i <luchadores.length; i++) {
